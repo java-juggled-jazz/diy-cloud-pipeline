b616a9f49c15adcae9d7f030f1349c2c637c91b4
Kamil <kamil.github.conv@gmail.com>
2024-04-27 22:19:00 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = var.availablity_zone
+}
+
+resource "yandex_vpc_network" "cloud-pipeline-net" {}
+
+resource "yandex_vpc_subnet" "cloud-pipeline-subnet" {
+  zone           = var.availablity_zone
+  network_id     = "${yandex_vpc_network.foo.id}"
+  v4_cidr_blocks = ["10.1.0.0/24"]
+}

8452d08483e6006493894ed578af6e6f2d13b9e6
Kamil <kamil.github.conv@gmail.com>
2024-04-28 00:10:12 +0300

    Create managed-k8s.tf

---
+++ terraform/managed-k8s.tf
@@ -0,0 +1,35 @@
+variable "service-account" {
+  type = string
+}
+
+variable "node-service-account" {
+  type = string
+}
+
+resource "yandex_kubernetes_cluster" "managed-k8s" {
+  name        = "managed-k8s"
+
+  network_id = yandex_vpc_network.cloud-pipeline-net.id
+
+  master {
+    version = "1.17"
+    zonal {
+      zone = var.availability_zone
+      subnet_id = yandex_vpc_network.cloud-pipeline-net.id
+    }
+
+    public_ip = false
+
+    security_group_ids = ["${yandex_vpc_security_group.security_group_name.id}"]
+  }
+
+  service_account_id = var.service-account.id
+  node_service_account_id = var.node-service-account.id
+
+  labels = {
+    my_key       = "my_value"
+    my_other_key = "my_other_value"
+  }
+
+  release_channel = "RAPID"
+}

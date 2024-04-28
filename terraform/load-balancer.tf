dd4c5e27c9d6b3479f7b9973146d161dd17af7a6
Kamil <kamil.github.conv@gmail.com>
2024-04-28 18:16:15 +0300

    Create load-balancer.tf

---
+++ terraform/load-balancer.tf
@@ -0,0 +1,11 @@
+resource "yandex_lb_network_load_balancer" "load-balancer" {
+  name = "load-balancer"
+
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+}

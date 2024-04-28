f5819a601e0ed12e324da275a7481372f7878293
Kamil <kamil.github.conv@gmail.com>
2024-04-28 19:19:13 +0300

    Update security-groups.tf

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -26,25 +26,3 @@ resource "yandex_vpc_security_group" "vm-security-group" {
     project_label = var.project_label
   }
 }
-
-resource "yandex_vpc_security_group" "k8s-lb-security-group" {
-  name        = "kubernetes-load-balancer-security-group"
-  network_id  = yandex_vpc_network.cloud-pipeline-net.id
-
-  ingress {
-    protocol       = "TCP"
-    description    = "HTTP"
-    v4_cidr_blocks = ["10.1.0.0/24"]
-    port           = 80
-  }
-  
-  egress {
-    protocol       = "ANY"
-    description    = "Outgoing packets"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  labels = {
-    project_label = var.project_label
-  }
-}

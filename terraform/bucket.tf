c81cc4d28fbd4b15bfd5f9956bc43b9d99828e1b
Kamil <kamil.github.conv@gmail.com>
2024-04-28 19:06:14 +0300

    Update bucket.tf

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -13,4 +13,8 @@ resource "yandex_storage_bucket" "pipeline-bucket" {
   }
   access_key = yandex_iam_service_account_static_access_key.bucket-sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.bucket-sa-static-key.secret_key
+
+  labels = {
+    project_label = var.project_label
+  }
 }

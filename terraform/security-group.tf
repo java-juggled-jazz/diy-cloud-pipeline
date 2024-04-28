fd1b1c1560f0b97abb6612a0fb049ddbea38f079
Kamil <kamil.github.conv@gmail.com>
2024-04-28 14:46:52 +0300

    Create security-group.tf

---
+++ terraform/security-group.tf
@@ -0,0 +1,22 @@
+resource "yandex_vpc_security_group" "vm-security-group" {
+  name        = "VM Security Group"
+  network_id  = yandex_vpc_network.cloud-pipeline-net.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH"
+    port           = 22
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "HTTP"
+    port           = 80
+  }
+  
+  egress {
+    protocol       = "ANY"
+    description    = "Outgoing packets"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}

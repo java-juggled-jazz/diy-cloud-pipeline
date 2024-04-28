5110fb4be8a87e3bf7449f14362a8e81e2b42d94
Kamil <kamil.github.conv@gmail.com>
2024-04-28 11:29:56 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+variable "availability_zone" {
+  type = string
+}
+
 terraform {
   required_providers {
     yandex = {
@@ -14,7 +18,7 @@ provider "yandex" {
 resource "yandex_vpc_network" "cloud-pipeline-net" {}
 
 resource "yandex_vpc_subnet" "cloud-pipeline-subnet" {
-  zone           = var.availablity_zone
+  zone           = var.availability_zone
   network_id     = yandex_vpc_network.cloud-pipeline-net.id
   v4_cidr_blocks = ["10.1.0.0/24"]
 }

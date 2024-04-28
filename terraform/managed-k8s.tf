c88559c89d8e7c984f198876df5dd4519b7ef8cd
Kamil <kamil.github.conv@gmail.com>
2024-04-28 19:26:02 +0300

    Update managed-k8s.tf

--- terraform/managed-k8s.tf
+++ terraform/managed-k8s.tf
@@ -12,7 +12,7 @@ resource "yandex_kubernetes_cluster" "managed-k8s" {
 
     public_ip = false
 
-//    security_group_ids = ["${yandex_vpc_security_group.security_group_name.id}"]
+    security_group_ids = [yandex_vpc_security_group.k8s-public-services.id]
   }
 
   service_account_id = var.service-account-id
@@ -23,4 +23,55 @@ resource "yandex_kubernetes_cluster" "managed-k8s" {
   }
 
   release_channel = "STABLE"
+}
+
+resource "yandex_vpc_security_group" "k8s-public-services" {
+  name        = "k8s-public-services"
+  network_id  = yandex_vpc_network.cloud-pipeline-net.id
+
+  ingress {
+    protocol          = "TCP"
+    description       = "The rule is allowing heathchecks from load balancer addresses range. It is needed for fault-tolerant-cluster working and load balancer services"
+    predefined_target = "loadbalancer_healthchecks"
+    from_port         = 0
+    to_port           = 65535
+  }
+
+  ingress {
+    protocol          = "ANY"
+    description       = "The rule is allowing master-node and node-node interaction inside security group"
+    predefined_target = "self_security_group"
+    from_port         = 0
+    to_port           = 65535
+  }
+
+  ingress {
+    protocol          = "ANY"
+    description       = "The rule is allowing pod-pod and service-service interaction"
+    v4_cidr_blocks    = concat(yandex_vpc_subnet.mysubnet.v4_cidr_blocks)
+    from_port         = 0
+    to_port           = 65535
+  }
+
+  ingress {
+    protocol          = "ICMP"
+    description       = "Service ICMP-packets from internal subnets are allowed"
+    v4_cidr_blocks    = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  }
+
+  ingress {
+    protocol          = "TCP"
+    description       = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+    from_port         = 30000
+    to_port           = 32767
+  }
+
+  egress {
+    protocol          = "ANY"
+    description       = "All egress traffic is allowed"
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 65535
+  }
 }

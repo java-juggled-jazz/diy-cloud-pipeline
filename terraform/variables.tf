64045139b3aec2e6327265213784463d00bc998c
Kamil <kamil.github.conv@gmail.com>
2024-04-28 16:00:48 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "availability_zone" {
+  type = string
+}
+
+variable "central-host-vars" {
+  type = object({
+    cores = number
+    core_fraction = number
+    memory = number
+    image_id = string
+  })
+}
+
+variable "pipeline-bucket" {
+  type = object({
+    max_size = number
+    default_storage_class = string
+  })
+}
+
+variable "service-account" {
+  type = string
+}
+
+variable "node-service-account" {
+  type = string
+}

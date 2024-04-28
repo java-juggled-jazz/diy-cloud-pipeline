24f203a16e5cf115a7a9cb79f1581be5a2f55ee8
Kamil <kamil.github.conv@gmail.com>
2024-04-28 15:03:40 +0300

    Update and rename security-group.tf to security-groups.tf

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -20,3 +20,20 @@ resource "yandex_vpc_security_group" "vm-security-group" {
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "yandex_vpc_security_group" "k8s-lb-security-group" {
+  name        = "Kubernetes Load Balancer Security Group"
+  network_id  = yandex_vpc_network.cloud-pipeline-net.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "HTTP"
+    port           = 80
+  }
+  
+  egress {
+    protocol       = "ANY"
+    description    = "Outgoing packets"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}

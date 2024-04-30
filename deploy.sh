cc3ea30788951ca6a4cc06d88e581da506f903cd
Kamil <kamil.github.conv@gmail.com>
2024-04-30 21:45:08 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,23 @@
 !#/bin/sh
 
+# HERE
+# IT GENERATES
+# SSH KEYS FOR VMS
+
 cd terraform
 terraform init
 terraform apply -auto-approve
 cd ..
+
+# HERE
+# IT CREATES
+# TEMPORARY BUILDER VM
+
+# ANSIBLE
+# IS
+# HERE
+
+# HERE
+# IT CREATES
+# BUILDER VM'S DISK SNAPSHOT
+# AND DESTROYS IT

c9ff29e3b459f7a93aa6e138e9aca26059da4f03
Kamil <kamil.github.conv@gmail.com>
2024-05-01 13:32:42 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,22 @@
 !#/bin/sh
 
+# Exporting Secrets
 source .secrets
 
+# Creating SSH Keys
 mkdir -p ~/.ssh/diy-cloud-pipeline-keys
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/diy-cloud-pipeline-keys/id_rsa -N "$PASSPHRASE"
 
+# Creating Cloud Resources
 cd terraform
 terraform init
 terraform apply -auto-approve
 cd ..
 
+# Exporting Terraform Outputs To Secrets
+terraform output -json | jq -r 'to_entries[] | .key + "=" + "\"" + (.value.value | tostring) + "\""' | while read -r line ; do echo export "$line"; done > env.sh && source env.sh
+
+# Creating Temporary Builder VM
 yc compute instance create \
   --name builder \
   --zone $AVAILABILITY_ZONE \
@@ -23,12 +30,14 @@ yc compute instance create \
 # IS
 # HERE
 
+# Creating Temporary Builder VM Snapshot
 yc compute snapshot create \
   --name builder-snapshot \
   --disk-id $BUILDER_DISK_ID \
   --labels $PROJECT_NAME_KEY=$PROJECT_NAME_VALUE \
   --async
 
+# Destroy Temporary Builder VM
 yc compute instance destroy \
   --id $BUILDER_VM_ID \
   --async

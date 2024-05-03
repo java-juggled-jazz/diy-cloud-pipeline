f24bc932b8d5861ad7f0dcbbb3225445db11f61b
Kamil <kamil.github.conv@gmail.com>
2024-05-04 01:14:50 +0300

    Update container-registry.tf

--- terraform/container-registry.tf
+++ terraform/container-registry.tf
@@ -2,6 +2,6 @@ resource "yandex_container_registry" "registry" {
   name = "container-registry"
   folder_id = var.folder_id
   labels = {
-    project_label = var.project_label
+    project-label = var.project_label
   }
 }

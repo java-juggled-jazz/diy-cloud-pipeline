d0506534d5bc01ef2995d91dd2bdfec26f62201d
Kamil <kamil.github.conv@gmail.com>
2024-05-04 00:18:03 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,13 @@ ssh-keygen -t rsa -b 2048 -f $BUILDER_HOST_SSH_KEY_DIR"id_rsa_builder" -N "$BUID
 # Creating Cloud Resources
 cd terraform
 terraform init
-terraform apply -auto-approve -var central_vm_ssh_key_dir=$BUILDER_HOST_SSH_KEY_DIR"id_rsa_builder.pub"
+terraform apply -auto-approve -var central_vm_ssh_key_dir=$BUILDER_HOST_SSH_KEY_DIR"id_rsa_builder.pub" \
+  -var cloud_id=$TF_VAR_cloud_id -var folder_id=$TF_VAR_folder_id \
+  -var availability_zone=$TF_VAR_availability_zone -var central_vm_cores=$TF_VAR_central_vm_cores \
+  -var central_vm_core_fraction=$TF_VAR_central_vm_core_fraction -var central_vm_memory=$TF_VAR_central_vm_memory \
+  -var central_vm_image_id=$TF_VAR_central_vm_image_id -var central_vm_disk_size=$TF_VAR_central_vm_disk_size \
+  -var central_vm_ssh_key_dir=$TF_VAR_central_vm_ssh_key_dir -var service_account_id=$TF_VAR_service_account_id \
+  -var project_label=$TF_VAR_project_label
 cd ..
 
 # Exporting Terraform Outputs To Secrets

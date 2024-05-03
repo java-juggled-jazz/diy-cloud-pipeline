f2aa4159854737477013c82e1fe48941fa244b04
Kamil <kamil.github.conv@gmail.com>
2024-05-03 20:03:58 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,24 @@
 !#/bin/sh
 
+# Declaring SSH Keys Dir Variables
+CENTRAL_HOST_SSH_KEY_DIR="~/.ssh/diy-cloud-pipeline-keys/id_rsa_central"
+BUILDER_HOST_SSH_KEY_DIR="~/.ssh/diy-cloud-pipeline-keys/id_rsa_builder"
+
 # Exporting Secrets
 source .env_vars
 
-# Creating SSH Keys
+# Creating Central Host SSH Key
 mkdir -p ~/.ssh/diy-cloud-pipeline-keys
-ssh-keygen -t rsa -b 2048 -f ~/.ssh/diy-cloud-pipeline-keys/id_rsa -N "$PASSPHRASE"
+ssh-keygen -t rsa -b 2048 -f $CENTRAL_HOST_SSH_KEY_DIR -N "$CENTRAL_HOST_PASSPHRASE"
+
+# Creating Builder Host SSH Key
+mkdir -p ~/.ssh/diy-cloud-pipeline-keys
+ssh-keygen -t rsa -b 2048 -f $BUILDER_HOST_SSH_KEY_DIR -N "$BUIDER_HOST_PASSPHRASE"
 
 # Creating Cloud Resources
 cd terraform
 terraform init
-terraform apply -auto-approve
+terraform apply -auto-approve -var central-vm-ssh-key-dir=$CENTRAL_HOST_SSH_KEY_DIR".pub"
 cd ..
 
 # Exporting Terraform Outputs To Secrets
@@ -23,13 +31,19 @@ yc compute instance create \
   --create-boot-disk image-id=$BUILDER_VM_IMAGE_ID,size=$BUILDER_VM_DISK_SIZE,type=network-ssd \
   --memory $BUILDER_VM_MEMORY --cores $BUILDER_VM_CORES --core-fraction $BUILDER_VM_CORE_FRACTION \
   --network-interface subnet-id=$SUBNET_ID \
+  --ssh-key $BUILDER_HOST_SSH_KEY_DIR".pub" \
   --format=yaml --no-user-output > ./outputs/builder-vm-output.yaml
 
 # Processing YAML-file to extract Builder VM ID, its Internal IP and its Boot Disk ID into a spectific file
-yq '("builder_vm_id: " + .id),("builder_vm_internal_ip: " + .network_interfaces[0].primary_v4_address.address),("boot_disk_id: " + .boot_disk.disk_id)' builder-vm-output.yaml -r > ./ansible/vars/builder_vm_vars.yaml
+yq '("builder_vm_id: " + .id),("builder_vm_internal_ip: " + .network_interfaces[0].primary_v4_address.address),("builder_vm_internal_ip: " + .network_interfaces[0].primary_v4_address.address),("boot_disk_id: " + .boot_disk.disk_id)' builder-vm-output.yaml -r > ./ansible/vars/builder_vm_vars.yaml
 
-# Exporting Builder VM
+# Exporting Builder VM ID, Internal IP and Boot Disk ID
 export BUILDER_VM_ID=$(yq -r '.id' ./ansible/vars/builder_vm_vars.yaml)
+export BUILDER_VM_INTERNAL_IP=$(yq -r '.builder_vm_internal_ip' ./ansible/vars/builder_vm_vars.yaml)
+export BUILDER_VM_BOOT_DISK_ID=$(yq -r '.boot_disk_id' ./ansible/vars/builder_vm_vars.yaml)
+
+# Setting IP-Addresses into Ansible Inventory file
+yq '.central.hosts."host-one" = "'$CENTRAL_HOST_IP'" | .builder.hosts."host-one" = "'$BUILDER_VM_INTERNAL_IP'"' ./ansible/inventory.yaml -y > ./ansible/inventory.yaml
 
 # Starting Playbook For Configuring Builder VM
 ansible-playbook builder-vm-configure.yaml -i inventory.yaml
@@ -37,7 +51,7 @@ ansible-playbook builder-vm-configure.yaml -i inventory.yaml
 # Creating Temporary Builder VM Snapshot
 yc compute snapshot create \
   --name builder-snapshot \
-  --disk-id $BUILDER_DISK_ID \
+  --disk-id $BUILDER_BOOT_DISK_ID \
   --labels $PROJECT_NAME_KEY=$PROJECT_NAME_VALUE \
   --format=yaml --no-user-output > ./outputs/builder-vm-snapshot-output.yaml
 

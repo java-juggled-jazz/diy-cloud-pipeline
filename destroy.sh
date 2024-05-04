69023470489b89eb0ea2cca7eb60460b87c3c325
Kamil <kamil.github.conv@gmail.com>
2024-05-04 13:52:03 +0300

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Declaring SSH Keys Dir Variables
+CENTRAL_HOST_SSH_KEY_DIR=$HOME"/.ssh/diy-cloud-pipeline-keys/"
+
 # Exporting Secrets
 source .env_vars
 
@@ -11,6 +14,6 @@ terraform destroy -auto-approve -var central_vm_ssh_key_dir=$BUILDER_HOST_SSH_KE
   -var availability_zone=$TF_VAR_availability_zone -var central_vm_cores=$TF_VAR_central_vm_cores \
   -var central_vm_core_fraction=$TF_VAR_central_vm_core_fraction -var central_vm_memory=$TF_VAR_central_vm_memory \
   -var central_vm_image_id=$TF_VAR_central_vm_image_id -var central_vm_disk_size=$TF_VAR_central_vm_disk_size \
-  -var service_account_id=$TF_VAR_service_account_id \
+  -var central_vm_ssh_key_dir=$CENTRAL_HOST_SSH_KEY_DIR"id_rsa_central.pub" -var service_account_id=$TF_VAR_service_account_id \
   -var project_label=$TF_VAR_project_label -var yandex_iam_token=$(yc iam create-token)
 cd ..

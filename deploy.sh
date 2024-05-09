7bb09959b530edc1ee0fe96bca78589c608d699e
Kamil <kamil.github.conv@gmail.com>
2024-05-10 00:52:43 +0300

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,13 @@
 # Checking The Flag
 case $1 in
 # Display Help Page
-    "help" || "h")
+    "help" | "h")
       cat README.md
       exit
     ;;
+
+    "" | "deploy" | "test")
+      ;;
 
 # Unrecognized Option
     *)
@@ -39,7 +42,7 @@ terraform init
 # Checking The Flag Again And Executing The Command Depending On Value
 case $1 in
 # Deploy with K8s
-  "" || "deploy")
+  "" | "deploy")
     terraform apply -auto-approve -var central_vm_ssh_key_dir=$BUILDER_HOST_SSH_KEY_DIR"id_rsa_builder.pub" \
       -var cloud_id=$TF_VAR_cloud_id -var folder_id=$TF_VAR_folder_id \
       -var availability_zone=$TF_VAR_availability_zone -var central_vm_cores=$TF_VAR_central_vm_cores \
@@ -49,9 +52,10 @@ case $1 in
       -var project_label=$TF_VAR_project_label -var yandex_iam_token=$(yc iam create-token)
     ;;
 
-# Deploy without K8s. Tests Only
+# Deploy without K8s. Tests Only. Don't Forget To Add Target Files After Adding Config Files
   "test")
-    terraform apply $(for var in $(ls | grep -e ".tf$" | grep -v "k8s"); do echo -n "-target=$var "; done;) \
+    cat $(for var in $(ls | grep -e ".tf$" | grep -v "k8s"); do echo -n "$var "; done;) > terraform-test.tf.temp
+    terraform apply -target="terraform-test.tf.temp" \
       -auto-approve -var central_vm_ssh_key_dir=$BUILDER_HOST_SSH_KEY_DIR"id_rsa_builder.pub" \
       -var cloud_id=$TF_VAR_cloud_id -var folder_id=$TF_VAR_folder_id \
       -var availability_zone=$TF_VAR_availability_zone -var central_vm_cores=$TF_VAR_central_vm_cores \
@@ -59,6 +63,7 @@ case $1 in
       -var central_vm_image_id=$TF_VAR_central_vm_image_id -var central_vm_disk_size=$TF_VAR_central_vm_disk_size \
       -var central_vm_ssh_key_dir=$CENTRAL_HOST_SSH_KEY_DIR"id_rsa_central.pub" -var service_account_id=$TF_VAR_service_account_id \
       -var project_label=$TF_VAR_project_label -var yandex_iam_token=$(yc iam create-token)
+    rm terraform-test.tf.temp
     ;;
 esac
 
